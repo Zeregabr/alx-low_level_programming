67388073638d335ec878b1abb3fd7827615710b5
Zeregabr <askelzky@gmail.com>
2022-12-20 23:37:45 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,6 +6,7 @@
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
+ * Return: the pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
@@ -13,7 +14,6 @@ char *_strcpy(char *dest, char *src)
 	int len, i;
 
 	len = 0;
-
 	while (src[len] != '\0')
 	{
 		len++;
@@ -23,7 +23,6 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-
 	dest[i] = '\0';
 	return (dest);
 }
