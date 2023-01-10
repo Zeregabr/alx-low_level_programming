834e89aa97e36e1fb52ba68060928cea0163115c
Zeregabr <askelzky@gmail.com>
2023-01-10 10:02:15 -0800

    f

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 /**
  * _memset - ironnou
  * @s: pointer to char params
- *  @b: data to change
- *  @n: index
- *  Return: *s
+ * @b: data to change
+ * @n: index
+ * Return: *s
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -12,13 +12,8 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-
 	{
-
 		s[i] = b;
-
 	}
-
 	return (s);
-
 }
