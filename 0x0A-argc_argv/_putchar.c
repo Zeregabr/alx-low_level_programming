37f1b8dd99e4bcdfa595335ea5facaf2e6021ee3
Zeregabr <askelzky@gmail.com>
2023-01-15 13:33:10 -0800

    d

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+#include "main.h"
+
+/**
+ * _putchar -  writes the character c to stdout
+ * @c:Ther character to print
+ * Return: on success 1.
+ * on error, -1 is returned and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
