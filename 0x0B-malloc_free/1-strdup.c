26c944ba62ae7886b941e1762c816be43bbcda28
Zeregabr <askelzky@gmail.com>
2023-01-16 03:27:03 -0800

    f

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,6 +16,7 @@ char *_strdup(char *str)char *_strdup(char *str)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		;
+
 	strout = (char *)malloc(sizeof(char) * (i + 1));
 
 	if (strout == NULL)
