68845b956a5cb8e5b04cd1d302eb9365c77ebf75
Zeregabr <askelzky@gmail.com>
2023-01-18 02:49:30 -0800

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -30,7 +30,7 @@ char **strtow(char *str)
 	char **aout;
 	unsigned int c, height, i, j, a1;
 
-	if (str == NULL || * str == '\0')
+	if (str == NULL || *str == '\0')
 		return (NULL);
 	for (c = height = 0; str[c] != '\0'; c++)
 	{
