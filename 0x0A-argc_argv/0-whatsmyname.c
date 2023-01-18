da99c6970eb62a670498517e4bc786d2c39bb6db
Zeregabr <askelzky@gmail.com>
2023-01-18 01:20:38 -0800

    1-args.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the program name, followed by a new
+ * @argc: The number of arguments supplied to the pr
+ * @argv: An array of pointers to the arguments
+ * Return: Always
+ */
+int main(int attribute((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
