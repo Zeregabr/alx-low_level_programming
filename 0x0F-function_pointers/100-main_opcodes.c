bf4ff7a28830021e0577719f27fe332173164465
Zeregabr <askelzky@gmail.com>
2023-01-18 22:24:55 -0800

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -21,3 +21,29 @@ void print_opcodes(char *a, int n)
 	}
 	printf("\n");
 }
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: O
+ */
+
+int main(int argc, char **argv)
+{
+	int a;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atoi(argv[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, a);
+	return (0);
+}
