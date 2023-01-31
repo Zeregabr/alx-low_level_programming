1418ab3f821af63c23c421ad5b1554d3dd81d976
Zeregabr <askelzky@gmail.com>
2023-01-31 12:58:45 -0800

    f

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,9 @@
-/**
- * * File: 0-print_listint.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
 #include <stdio.h>
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: A pointer to the head of the list_t list.
- * print_listint: akkask
- * h:asaklhsalsk
  * Return: The number of nodes in the list_t list.
  */
 
