e2b114c79bbde63f73a120f7a75e032a9134cb5d
Zeregabr <askelzky@gmail.com>
2023-02-02 07:25:14 -0800

    f

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,22 +1,45 @@
 #include "lists.h"
 
 /**
- * sum_listint - Calculates the sum of all the
- * data (n) of a listint_t list.
- * @head: A pointer to the head of the listint_t list.
- * Return: If the list is empty - 0.
- * Otherwise - the sum of all the data.
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ * list at a given position.
+ * @head: A pointer to the address of the
+ * head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ * node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ * Return: If the function fails - NULL.
+ * Otherwise - the address of the new node.
  */
 
-int sum_listint(listint_t *head)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int sum = 0;
+	listint_t *new, *copy = *head;
+	unsigned int node;
 
-	while (head)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
 	{
-		sum += head->n;
-		head = head->next;
+		new->next = copy;
+		*head = new;
+		return (new);
 	}
 
-	return (sum);
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (copy == NULL || copy->next == NULL)
+			return (NULL);
+
+		copy = copy->next;
+	}
+
+	new->next = copy->next;
+	copy->next = new;
+
+	return (new);
 }
