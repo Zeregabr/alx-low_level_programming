8be47a3000aab8159926f2bc257c94bba12d1f34
Zeregabr <askelzky@gmail.com>
2023-02-02 21:54:53 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,8 +3,8 @@
 /**
  * binary_to_uint - converts a binary number to an
  * unsigned int.
- * @b: binary.
- * Return: unsigned int.
+ * @b: binary
+ * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -17,6 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	ui = 0;
 	for (len = 0; b[len] != '\0'; len++)
 		;
+	
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
@@ -28,5 +29,6 @@ unsigned int binary_to_uint(const char *b)
 			ui += base_two;
 		}
 	}
+	
 	return (ui);
 }
