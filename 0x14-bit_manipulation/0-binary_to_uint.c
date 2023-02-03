70760581688ca73931fb2179b0ec9439aa367165
Zeregabr <askelzky@gmail.com>
2023-02-02 21:47:11 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,19 +17,16 @@ unsigned int binary_to_uint(const char *b)
 	ui = 0;
 	for (len = 0; b[len] != '\0'; len++)
 		;
-
-		for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
 		{
-			if (b[len] != '0' && b[len] != '1')
-			{
-				return (0);
-			}
-
-			if (b[len] & 1)
-			{
-				ui += base_two;
-			}
+			return (0);
 		}
-
-		return (ui);
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
+	return (ui);
 }
