46761017abf604eabef400cbcdd60305d7accc15
Zeregabr <askelzky@gmail.com>
2023-02-02 21:42:34 -0800

    f

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,6 @@
  * @b: binary.
  * Return: unsigned int.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
@@ -16,12 +15,10 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 
 	ui = 0;
-
 	for (len = 0; b[len] != '\0'; len++)
 		;
 
 		for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-
 		{
 			if (b[len] != '0' && b[len] != '1')
 			{
