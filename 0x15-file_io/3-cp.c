2e2a7ad1b6fd545ee2cb7ec17b89b737953bb916
Zeregabr <askelzky@gmail.com>
2023-02-09 19:41:27 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,28 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ * Return: A pointer to the newly-allocated buffer.
+ */
+
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
  * close_file - Closes file descriptors.
  * @fd: The file descriptor to be closed.
  */
